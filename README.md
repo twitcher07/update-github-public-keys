b288919a1cb24f153b0f16ffce91ee4f343f92be
Tyrel Witcher <tyrel.witcher@gmail.com>
2023-04-19 11:18:09 -0500

    added bash script file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# update github public keys
- 
+### How to use:
+
+Update known_hosts for current user:
+
+```
+curl -fsSL https://raw.githubusercontent.com/twitcher07/update-github-public-keys/main/update_keys.sh | bash 
+```
+
+or update known_hosts for different user (when using root):
+
+```
+curl -fsSL https://raw.githubusercontent.com/twitcher07/update-github-public-keys/main/update_keys.sh | bash -s -- [path/to/user]/.ssh/known_hosts
+```
---
+++ update_keys.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+if [ "$1" ]; then
+
+  KNOWN_HOSTS_FILE=$1
+  
+  echo "$KNOWN_HOSTS_FILE"
+
+    if [ ! -f "$KNOWN_HOSTS_FILE" ]; then
+
+      echo "$1 does not exist."
+      exit 1
+  
+    else 
+
+      # First we remove any keys that exists
+      ssh-keygen -R github.com -f $KNOWN_HOSTS_FILE
+
+      # Then we update the new ones
+      curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> $KNOWN_HOSTS_FILE
+
+      # Then we hash the hostnames
+      ssh-keygen -Hf $KNOWN_HOSTS_FILE
+  
+    fi
+  
+else
+
+  # First we remove any keys that exists
+  ssh-keygen -R github.com
+
+  # Then we update the new ones
+  curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts
+
+  # Then we hash the hostnames
+  ssh-keygen -Hf ~/.ssh/known_hosts
+
+fi
+
+echo "\n----------------------------"
+echo "Updated github.com keys."
+echo "\n----------------------------"
