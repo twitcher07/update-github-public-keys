99dd5b3198f058c7e5909c4ab67972b7ca28825e
Tyrel Witcher <tyrel.witcher@gmail.com>
2023-06-15 09:00:36 -0500

    fix spelling error.

--- remove_ips.sh
+++ remove_ips.sh
@@ -20,5 +20,5 @@ fi
 for ip in $(for i in $(seq -f "140.82.%g.%%g" 112 127); do seq -f $i 1 254; done); do ssh-keygen -R $ip -f $KNOWN_HOSTS_FILE; done
 
 echo -e "\n----------------------------"
-echo "Finished reemovin github IP address keys."
+echo "Finished reemoving github IP address keys."
 echo -e "----------------------------"
